c9ed67e29b36518bba0c25ed5441ba3af6e438f2
stonecraftwolf <stonecraftwolf@gmail.com>
2016-07-04 16:26:49 -0400

    all done exit
    exit

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  variable=value
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
-
+def greeting(name, title)
+	puts "Hello #{name}, you #{title}."
 end
 
 def return_a_value
-
+	"Nice"
 end
 
 def last_evaluated_value
-
+	"Nice"
+	"expert"
 end
 
-def pizza_party
-
+def pizza_party(pizza="cheese")
+	pizza
 end
